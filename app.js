bf2b0b15a73a6e28b41cfd17af2810b5fea2429e
Michael Graybeal <michael@thegraybeals.com>
2014-10-05 13:01:41 -0600

    Mostly done already

---
+++ app.js
@@ -0,0 +1,60 @@
+var http = require('http'),
+	fs = require('fs'),
+	RSS = require('rss'),
+	data = require('./data');
+
+var server = http.createServer(function(req,res){
+	var path = req.url;
+	if(path == '/rss'){
+		generateRss(res);
+	} else{
+		res.redirect('https://www.lds.org/general-conference?lang=eng');
+	}
+});
+
+var generateRss = function(res){
+	var now = new Date();
+	fs.readFile(__dirname + '/data.json', function(err, data){
+		if(err)
+			throw err;
+		var dataArray = JSON.parse(data);
+		dataArray = dataArray.select(function(item){
+			item.date = new Date(item.date);
+			return item;
+		});
+		dataArray = dataArray.where(function(item){
+			return now > item.date;
+		});
+		var rss = new RSS({
+			title: 'Daily Conference Talks',
+			feed_url: 'thegraybeals.com',
+			site_url: 'thegraybeals.com'
+		});
+		for(var i = 0; i < dataArray.length; i++){
+			rss.item(dataArray[i]);
+		}
+		res.writeHead(200, {"Content-Type": "rss/xml"});
+		res.write(rss.xml());
+		res.end();
+	});
+}
+
+server.listen(process.env.PORT || 8081);
+console.log('waiting');
+
+Array.prototype.select = function(callback){
+	var result = [];
+	for (var i = 0; i < this.length; i++) {
+		result.push(callback(this[i]));
+	};
+	return result;
+};
+
+Array.prototype.where = function(callback){
+	var result = [];
+	for (var i = 0; i < this.length; i++) {
+		if(callback(this[i]))
+			result.push(this[i]);
+	};
+	return result;
+};
---
+++ scrape.js
@@ -0,0 +1,23 @@
+
+var generateData = function(dateIn){
+	var data = [], today = dateIn || new Date();
+	$('.sessions tr:has(span.talk)').each(function(i,el){
+		var title, author, link, description, $el = $(el), date;
+		title = $el.find('span.talk a').text();
+		author = $el.find('span.speaker').text();
+		link = $el.find('.audio-mp3').attr('href').split('?')[0];
+		description = 'Conference talk ' + author;
+		today.setDate(today.getDate()+1);
+		date = new Date(today);
+		data.push({
+			title: title,
+			author: author,
+			description: description,
+			url: link,
+			guid: link,
+			date: date
+		});
+	});
+
+	console.log(JSON.stringify(data));
+};
