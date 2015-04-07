97746919f80b4e073cd5095149d95f32d46c036e
Michael Graybeal <michael@thegraybeals.com>
2015-04-06 23:54:46 -0600

    April Conference!

--- scrape.js
+++ scrape.js
@@ -3,9 +3,14 @@ var generateData = function(dateIn){
 	var data = [], today = dateIn || new Date();
 	$('.sessions tr:has(span.talk)').each(function(i,el){
 		var title, author, link, description, $el = $(el), date;
-		title = $el.find('span.talk a').text();
+		title = $el.find('span.talk a').text() || $el.find('span.talk').text();
 		author = $el.find('span.speaker').text();
 		link = $el.find('.audio-mp3').attr('href').split('?')[0];
+		if(title === 'The Sustaining of Church Officers' ||
+			title.indexOf('Church Auditing Department') > -1 ||
+			title.indexOf('Statistical Report') > -1){
+			return;
+		}
 		description = author;
 		today.setDate(today.getDate()+1);
 		date = new Date(today);
